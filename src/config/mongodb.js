204fdf6e9f87505b33ee1c0d8b61cefa4aef6075
thanhleesenpai <thanhle20072004@gmail.com>
2025-03-12 11:51:34 +0700

    connect mongodb env variables

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,39 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+// thanhleesenpai
+//uOlGkwX79D3oVD2X
+
+const MONGODB_URI = 'mongodb+srv://thanhleesenpai:uOlGkwX79D3oVD2X@cluster0-thanhlee.ibb2t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-Thanhlee'
+
+const DATABASE_NAME = 'trello-thanhlee-mern-stack'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatabaseInstance = null
+
+// Khởi tạo một đối tượng MongoClientInstance để connect tới MongoDB
+const MongoClientInstance = new MongoClient(MONGODB_URI, {
+  //Lưu í cái server API có từ phiên bản MongoDB 5.0.0 trở lên, có thể không cần dùng nó, còn nếu dùng nó là chúng ta sẽ chỉ định một Stable API Vesion của MongoDB
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  // Gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân của MongoClientInstance
+  await MongoClientInstance.connect()
+
+  // Kết nối thành công thì lấy ra Database theo tên và gán ngược nó lại vào biến trelloDatabaseInstance ở trên của chúng ta
+  trelloDatabaseInstance = MongoClientInstance.db(DATABASE_NAME)
+}
+
+// Function GET_DB (không async) này có nhiệm vụ ẽport ra cái Trello Database instance sau khi đã connect thành công tới MongoDB để chúng ta sử dụng ở nhiều nơi khác nhau trong code.
+// Lưu ý phải đảm bảo chỉ luôn gọi cái GET_DB này sau khi đã kết nối thành công tới MongoDB
+export const GET_DB =() => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first!')
+  return trelloDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -1,33 +1,44 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, GET_DB } from '~/config/mongodb'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  app.listen(port, hostname, () => {
+    console.log(`3. Hello, I am running at http://${ hostname }:${ port }/`)
+  })
+}
+
+//Chỉ khi kết nối tới Database thành công thì mới Start Server Backend lên
+// Immediately-invoked / Anonymous AsyncFunctions (IIFE)
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB Cloud Atlas...')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB Cloud Atlas!')
+
+    //Khởi động Server Back-end sau khi Connect Database thành công
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+// //Chỉ khi kết nối tới Database thành công thì mới Start Server Backend lên
+// console.log('1. Connecting to MongoDB Cloud Atlas...')
+// CONNECT_DB()
+//   .then(() => console.log('Connected to MongoDB Cloud Atlas!'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
