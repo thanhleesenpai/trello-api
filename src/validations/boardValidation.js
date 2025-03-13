52492256822ea64141c45d3a08bd2220dd94ca3d
thanhleesenpai <thanhle20072004@gmail.com>
2025-03-13 19:04:14 +0700

    validation with joi

---
+++ src/validations/boardValidation.js
@@ -0,0 +1,36 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => {
+
+  //Mặc định chúng ta không cần cusom message ở phía BE vì để cho Front-end tự validate và custom message phía FE cho đẹp
+  // Back_end chỉ cần validate đảm bảo DỮ LIỆU CHUẨN XÁC, và trả về message mặc định từ thư viện là được
+  // Quan trọng: Việc validate dữ liệu BẮT BUỘC phải có ở phía Back-end vì đây là điểm cuối để lưu trữ dữ liệu vào Database
+  // Thông thường trong thực tế, điều tốt nhất cho hệ thống là hãy luôn validate dữ liệu ở cả Back-end và Front-end
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
+      'any.required': 'Title is required',
+      'string.empty': 'Title is not allowed to be empty',
+      'string.min': 'Title length must be at least 3 characters long',
+      'string.max': 'Title length must be less than or equal to 50 characters long',
+      'string.trim': 'Title must not have leading or trailing whitespace'
+    }),
+    description: Joi.string().required().min(3).max(256).trim().strict()
+  })
+
+  try {
+    console.log('req.body: ', req.body)
+    // Chỉ định abortEarly: false để tr hợp có nhiều lỗi thì trả về tất cả lỗi
+    await correctCondition.validateAsync(req.body, { abortEarly: false })
+    //next()
+    res.status(StatusCodes.CREATED).json({ message: 'POST from validation: APIs create new board' })
+  } catch (error) {
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: new Error(error).message
+    })
+  }
+}
+
+export const boardValidation = {
+  createNew
+}
