d6a3feeb0d374b7a54de1aed2ecc958b0c6e8726
thanhleesenpai <thanhle20072004@gmail.com>
2025-03-16 18:34:33 +0700

    validate model

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -11,11 +11,9 @@ const createNew = async (reqBody) => {
     }
     // Gọi tới tầng model để xử lí lưu bản ghi newBoard vào trong Database
     const createdBoard = await boardModel.createNew(newBoard)
-    console.log(createdBoard)
 
     // Lấy bản ghi board sau khi gọi (tùy mục đích dự án mà có cần bước này hay không)
     const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
-    console.log(getNewBoard)
 
     // Làm thêm các xử lí logic khác với các collection khác tùy đặc thù dự án...
     // Bắn email, notification về cho admin khi có một cái board mới được tạo...
