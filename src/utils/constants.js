ce9fcf02de0e09cabe5265a3f3fa6ccf0e008af8
thanhleesenpai <thanhle20072004@gmail.com>
2025-03-29 17:34:24 +0700

    update whilelist domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
   //'http://localhost:5173' // Không cần localhost nữa vì ở file config/cors đã luôn cho phép các môi trường dev (env.BUILD_MODE === 'dev')
+  'https://trello-web-ten-kappa.vercel.app'
 ]
 
 export const BOARD_TYPES = {
